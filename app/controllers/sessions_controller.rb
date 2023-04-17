87a491ed3c5c92a89031d90bbe7b80343e7d2908
Shad Itschner <helbnt@gmail.com>
2023-04-17 15:04:13 -0400

    merge

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
 def new
 end
 
-  def create
+def create
   user = User.find_by(email: params[:session][:email].downcase)
   if user && user.authenticate(params[:session][:password])
     reset_session
+    remember user
     log_in user
     redirect_to user
   else
@@ -15,8 +16,8 @@ end
   end
 end
 
-def destroy
-  log_out
-  redirect_to root_url, status: :see_other
-end
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url, status: :see_other
+  end
 end
--- db/migrate/20230416185003_add_website_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddWebsiteToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :url, :string
-  end
-end
